04dd5c03929176595c343c1076dfa90e186461c4
fng3r <dinvk@mail.ru>
2017-10-25 23:14:41 +0500

    extract Vertex to a new file

---
+++ Vertex.cs
@@ -0,0 +1,20 @@
+﻿namespace ca
+{
+    public class Vertex
+    {
+        public readonly int Number;
+        public readonly int X;
+        public readonly int Y;
+        public Vertex(int number, int x, int y)
+        {
+            Number = number;
+            X = x;
+            Y = y;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("V{0} ({1}, {2})", Number, X, Y);
+        }
+    }
+}
